f9edbf1d4513e711fe634fe3a1041d3bc2f859af
Bruce S <70772398+KosLD@users.noreply.github.com>
2021-11-30 13:17:30 -0500

    v0.2

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,4 +1,11 @@
-# My First PyGame, Bruce Smith, 11/30/21, 12:20PM, v0.1
+# My First PyGame, Bruce Smith, 11/30/21, 1:16PM, v0.2
 
 import pygame, sys
 from pygame.locals import *
+
+# Initialize PyGame
+pygame.init()
+
+# Setup the Window to draw on.
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('My First PyGame')
