eabe1230226410dd9702662a317a8859d8026f47
Bruce S <70772398+KosLD@users.noreply.github.com>
2021-11-30 13:37:20 -0500

    v0.5
    Added window background color.

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Bruce Smith, 11/30/21, 1:28PM, v0.4
+# My First PyGame, Bruce Smith, 11/30/21, 1:36PM, v0.5
 
 import pygame, sys
 from pygame.locals import *
@@ -10,12 +10,14 @@ pygame.init()
 windowSurface = pygame.display.set_mode((500, 400), 0, 32)
 pygame.display.set_caption('My First PyGame')
 
+
 # Setup color values.
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-GIGGIDY_GIGIDY_GOOO = (239, 51, 37)
+BLUE = (0, 0, 255)
+GIGGIDYGIGIDYGOOO = (239, 51, 37)
 
 # Setup the fonts.
 basicFont = pygame.font.SysFont(None, 48)
@@ -25,3 +27,6 @@ text = basicFont.render('Hello, world!', True, WHITE, BLUE)
 textRect = text.get_rect()
 textRect.centerx = windowSurface.get_rect().centerx
 textRect.centery = windowSurface.get_rect().centery
+
+# Fill in window background color.
+windowSurface.fill(GIGGIDYGIGIDYGOOO)
