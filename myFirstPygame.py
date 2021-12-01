176bf81109b4094dbcaf02e182dce70a8adc7378
Bruce S <70772398+KosLD@users.noreply.github.com>
2021-12-01 08:41:09 -0500

    v0.6

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Bruce Smith, 11/30/21, 1:36PM, v0.5
+# My First PyGame, Bruce Smith, 11/30/21, 8:49AM, v0.6
 
 import pygame, sys
 from pygame.locals import *
@@ -9,7 +9,6 @@ pygame.init()
 # Setup the Window to draw on.
 windowSurface = pygame.display.set_mode((500, 400), 0, 32)
 pygame.display.set_caption('My First PyGame')
-
 
 # Setup color values.
 BLACK = (0, 0, 0)
@@ -30,3 +29,28 @@ textRect.centery = windowSurface.get_rect().centery
 
 # Fill in window background color.
 windowSurface.fill(GIGGIDYGIGIDYGOOO)
+
+# Draw a green polygon onto the surface.
+
+
+
+
+
+
+
+# Draw a circle.
+pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
+
+# Draw an ellipse.
+pygame.draw.ellipse(windowSurface, RED, (300, 250, 40 80), 1)
+
+#Draw text background rectangle onto surface. NEW STARTING WEDNESDAY
+pygame.draw.rect(windowSurface, RED, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
+
+# Get a pixel array of the surface.
+pixArray = pygame.PixelArray(windowSurface)
+pixArray[480][380] = BLACK
+del pixArray
+
+# Draw the text onto the surface.
+windowSurface.blit(text, textRect)
